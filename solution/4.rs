0341a740037e50025e62db751b302ed11f567c8d
YDZ <ydz627@gmail.com>
2017-09-01 19:42:09 +0800

    solution4

---
+++ solution/4.rs
@@ -0,0 +1,61 @@
+/**
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Example:
+ *
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// discuss: https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let seq: Vec<char> = s.chars().collect();
+        let len = seq.len();
+        let (mut start, mut end, mut max) = (0, 0, 0);
+
+        while end < len {
+            for idx in start..end {
+                if seq[end] == seq[idx] {
+                    start = idx + 1;
+                    break;
+                }
+            }
+            let curr = end - start + 1;
+            if curr > max {
+                max = curr
+            }
+            end += 1
+        }
+        max as i32
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_3() {
+        assert_eq!(
+            Solution::length_of_longest_substring("abcabcbb".to_string()),
+            3
+        );
+        assert_eq!(Solution::length_of_longest_substring("bbbb".to_string()), 1);
+        assert_eq!(
+            Solution::length_of_longest_substring("pwwkew".to_string()),
+            3
+        );
+    }
+}
