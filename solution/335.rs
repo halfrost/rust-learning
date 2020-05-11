2c401387eb0e001f33037519358b1ce9140459bb
YDZ <ydz627@gmail.com>
2020-05-11 20:20:20 +0800

    solution335

---
+++ solution/335.rs
@@ -0,0 +1,45 @@
+/**
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// discuss: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+// TODO
+impl Solution {
+    pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        vec![]
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_34() {}
+}
