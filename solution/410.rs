cf004750666f28947122952951ed2b1502641b1f
YDZ <ydz627@gmail.com>
2020-05-26 20:20:20 +0800

    solution410

---
+++ solution/410.rs
@@ -0,0 +1,89 @@
+/**
+ * [9] Palindrome Number
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 121
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ *
+ *
+ * Follow up:
+ *
+ * Coud you solve it without converting the integer to a string?
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/palindrome-number/
+// discuss: https://leetcode.com/problems/palindrome-number/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+// TODO: not optimal, we only have to revert half of the string
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+        let mut digits: Vec<i32> = Vec::new();
+        let mut input = x;
+        while input != 0 {
+            digits.push(input % 10);
+            input = input / 10;
+        }
+        let len = digits.len();
+        // handle one digit
+        if len < 2 {
+            return true;
+        }
+        // handle end with 0
+        if digits[0] == 0 {
+            return false;
+        }
+        let mut i = 0;
+        while i < len / 2 {
+            if digits[i] != digits[len - 1 - i] {
+                return false;
+            }
+            i += 1;
+        }
+        true
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_9() {
+        assert_eq!(Solution::is_palindrome(-32), false);
+        assert_eq!(Solution::is_palindrome(10), false);
+        assert_eq!(Solution::is_palindrome(0), true);
+        assert_eq!(Solution::is_palindrome(9), true);
+        assert_eq!(Solution::is_palindrome(121), true);
+        assert_eq!(Solution::is_palindrome(2222), true);
+        assert_eq!(Solution::is_palindrome(11222211), true);
+    }
+}
