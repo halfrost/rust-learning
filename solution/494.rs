1d61dd4d5d384fc32de0b3a7227e8afbfc2e1393
YDZ <ydz627@gmail.com>
2020-06-12 20:20:20 +0800

    solution494

---
+++ solution/494.rs
@@ -0,0 +1,35 @@
+/**
+ * [93] Restore IP Addresses
+ *
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ *
+ * Example:
+ *
+ *
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ *
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/restore-ip-addresses/
+// discuss: https://leetcode.com/problems/restore-ip-addresses/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn restore_ip_addresses(s: String) -> Vec<String> {
+        vec![]
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_93() {}
+}
