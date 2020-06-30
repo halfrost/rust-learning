f525ae6d6d97396e21b9152e50bc8e73107c68e1
YDZ <ydz627@gmail.com>
2020-06-30 20:20:20 +0800

    solution582

---
+++ solution/582.rs
@@ -0,0 +1,40 @@
+/**
+ * [214] Shortest Palindrome
+ *
+ * Given a string s, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "aacecaaa"
+ * Output: "aaacecaaa"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "abcd"
+ * Output: "dcbabcd"
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/shortest-palindrome/
+// discuss: https://leetcode.com/problems/shortest-palindrome/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn shortest_palindrome(s: String) -> String {
+        "".to_owned()
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_214() {}
+}
