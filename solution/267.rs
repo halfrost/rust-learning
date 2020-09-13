8c8f1b5292fc36f628fa5715bffe7e6a5693a510
halfrost <ydz627@gmail.com>
2020-09-14 04:45:42 +0800

    Update 267.rs

--- solution/267.rs
+++ solution/267.rs
@@ -5,6 +5,7 @@
  *
  * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
  *
+ 
  * You may assume the integer does not contain any leading zero, except the number 0 itself.
  *
  * Example 1:
