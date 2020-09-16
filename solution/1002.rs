582090cb6b0f643026148ebe5273e100b268fd07
YDZ <ydz627@gmail.com>
2020-09-16 08:57:38 +0800

    add solution 1002

---
+++ solution/1002.rs
@@ -0,0 +1,36 @@
+/**
+ * [233] Number of Digit One
+ *
+ * Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+ *
+ * Example:
+ *
+ *
+ * Input: 13
+ * Output: 6
+ * Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+ *
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/number-of-digit-one/
+// discuss: https://leetcode.com/problems/number-of-digit-one/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn count_digit_one(n: i32) -> i32 {
+        0
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_233() {}
+}
